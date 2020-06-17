d0656b22e14de58ce44f15735cf18138b724bfd5
oltov <ururairurai@gmail.com>
2020-06-17 23:11:41 +0300

    добавляет блок цели на год

--- source/js/modules/header-event.js
+++ source/js/modules/header-event.js
@@ -1,7 +1,6 @@
 const eventHeader = () => {
   const title = document.querySelector('.header-content__title');
   const subTitle = document.querySelector('.header-content__wrapper');
-  console.log(13, title)
     setTimeout(() => {
       title.classList.add('header-content__title--event');
       subTitle.classList.add('header-content__wrapper--event');
--- source/js/modules/slider-swiper.js
+++ source/js/modules/slider-swiper.js
@@ -1,7 +1,9 @@
 
 const initSliderSwiper = () => {
 
-  const slider = document.querySelector(`.swiper-container`);
+  const slider = document.querySelector(`#main-slider`);
+  const goals = document.querySelector(`#goals-slider`);
+  const goalsTabs = document.querySelectorAll(`[data-goal-index]`);
 
   const newSlider = new Swiper(slider, {
     slidesPerView: 1,
@@ -20,6 +22,33 @@ const initSliderSwiper = () => {
       1024: {
         slidesPerView: 2,
       }
+    }
+  });
+
+  const toggleSwitch = () => {
+    if(goalsTabs) {
+      goalsTabs.forEach(toggle => {
+        toggle.addEventListener('click', (evt) => {
+          goalsTabs.forEach(btn => {
+            btn.classList.remove('slider-btn__toggle--active-goal')
+          });
+          goalsSlider.slideTo(evt.target.getAttribute('data-goal-index'));
+          evt.target.classList.add('slider-btn__toggle--active-goal')
+          console.log(evt.target.getAttribute('data-goal-index'))
+        })
+      })
+    }
+  };
+
+  toggleSwitch();
+
+  const goalsSlider = new Swiper(goals, {
+    slidesPerView: 1,
+    speed: 600,
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
     }
   });
 };
